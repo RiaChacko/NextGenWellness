37a45e5560d6414535cfdc45d13a9e224b243e18
Ria Chacko <chacko.ria2003@gmail.com>
2025-02-07 23:47:47 -0500

    Set up routing for pages and added basic login form using tailwind

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Landing from './pages/Landing.jsx';
+import Login from './pages/Login.jsx';
+import SignUp from './pages/SignUp.jsx';
+import Dashboard from './pages/Dashboard.jsx';
+import Profile from './pages/Profile.jsx';
+import GoalSetting from './pages/GoalSetting.jsx';
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Landing/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/signup' element={<SignUp/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/goals' element={<GoalSetting/>}/>
+        </Routes>
+
+      </Router>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,12 @@
+
+import "./Landing.css";
+function Landing() {
+return(
+    <div className="text-3xl font-extrabold underline">
+        <h1>Landing Page</h1>
+    </div>
+);
+    
+}
+
+export default Landing
