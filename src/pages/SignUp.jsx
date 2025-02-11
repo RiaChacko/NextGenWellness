e516377e218122788c7d6bac4103e63176c77687
aneeshaa1 <acharyannie@gmail.com>
2025-02-11 18:20:51 -0500

    aneesha commit

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,6 +2,7 @@ import logo from "../assets/login-logo.png";
 import { Link } from "react-router-dom";
 function SignUp () {
     return(
+        <h2>hiiiiiiiiiiiii</h2>
         <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
             <img class="mx-auto h-10 w-auto" src={logo} alt="Your Company"/>
