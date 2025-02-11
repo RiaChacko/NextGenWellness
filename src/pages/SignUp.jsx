de9e4ac53bae71223383f304e2eb9298ac542844
aneeshaa1 <acharyannie@gmail.com>
2025-02-11 18:22:59 -0500

    hello

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,8 +2,9 @@ import logo from "../assets/login-logo.png";
 import { Link } from "react-router-dom";
 function SignUp () {
     return(
-        <h2>hiiiiiiiiiiiii</h2>
+        
         <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+          <h2>HELLO</h2>
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
             <img class="mx-auto h-10 w-auto" src={logo} alt="Your Company"/>
             <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Register for Account</h2>
