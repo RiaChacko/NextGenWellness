371a756e2617ce01cf951543eb08d733efd33cb2
Ria Chacko <chacko.ria2003@gmail.com>
2025-02-11 18:17:29 -0500

    Added name field to sign up page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -16,6 +16,12 @@ function SignUp () {
           <input type="email" name="email" id="email" autocomplete="email" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"/>
         </div>
       </div>
+      <div>
+        <label for="email" class="block text-sm/6 font-medium text-gray-900">Name</label>
+        <div class="mt-2">
+          <input type="name" name="name" id="name" autocomplete="name" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"/>
+        </div>
+      </div>
 
       <div>
         <div class="flex items-center justify-between">
@@ -30,7 +36,7 @@ function SignUp () {
         
         <div class="mt-2">
         <label for="password" class="block text-sm/6 font-medium text-gray-900">Confirm Password</label>
-          <input type="password" name="password" id="password" autocomplete="current-password" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"/>
+          <input type="password" name="password" id="confirmpassword" autocomplete="current-password" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"/>
         </div>
         
       </div>
