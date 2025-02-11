30ed0f1e10d1051cc96cabf69d667d9c4dfbb9dc
Ria Chacko <chacko.ria2003@gmail.com>
2025-02-11 18:27:00 -0500

    Login branch intial commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -20,7 +20,7 @@ function Login () {
 
       <div>
         <div class="flex items-center justify-between">
-          <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
+          <label for="password" class="block text-sm/6 font-medium text-gray-900">Pass</label>
           <div class="text-sm">
             <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
           </div>
