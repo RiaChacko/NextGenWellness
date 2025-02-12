fe5b0cf8c3fe7bd0681d3b7b3dec0bcd5b3c69b4
Ria Chacko <chacko.ria2003@gmail.com>
2025-02-11 23:32:20 -0500

    Styled the login page to match wireframe

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,32 +1,38 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/login-logo.png";
+import "../pages/Login.css";
 function Login () {
     return(
 
-    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+    <div class="entire-pg">
+        <div class="top-header-form">
             <img class="mx-auto h-10 w-auto" src={logo} alt="Your Company"/>
-            <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Sign in to your account</h2>
+            <h2>NEXT-GEN &nbsp; WELLNESS</h2>
+            
+            
     </div>
-
-    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    <div className="buttons">
+      <button>SIGN UP</button>
+      <button id="selected">LOGIN</button>
+    </div>
+    <div class="form-field">
         <form class="space-y-6" action="#" method="POST">
     <div>
-        <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
+        {/* <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label> */}
         <div class="mt-2">
-          <input type="email" name="email" id="email" autocomplete="email" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"/>
+          <input type="email" placeholder="Email Address" name="email" id="email" autocomplete="email" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"/>
         </div>
       </div>
 
       <div>
         <div class="flex items-center justify-between">
-          <label for="password" class="block text-sm/6 font-medium text-gray-900">Pass</label>
+          {/* <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label> */}
           <div class="text-sm">
             <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
           </div>
         </div>
         <div class="mt-2">
-          <input type="password" name="password" id="password" autocomplete="current-password" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"/>
+          <input type="password" placeholder="Password" name="password" id="password" autocomplete="current-password" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"/>
         </div>
       </div>
 
