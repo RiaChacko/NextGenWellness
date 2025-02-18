d504a9fecca934001b564113c99b24c162b418bd
aneeshaa1 <acharyannie@gmail.com>
2025-02-17 23:23:14 -0500

    fixed google signup styling

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import logo from "../assets/welcome-to.svg";
 import { Link } from "react-router-dom";
 import "../pages/SignUp.css";
+import google from "../assets/google-lg.png";
 
 
 function SignUp () {
@@ -25,18 +26,31 @@ function SignUp () {
 
           <div className="text-input center">
             <form action="/submit" method="POST">
+
               <div className="center">
                 <input type="name" name="name" id="name" placeholder="Name" size={40} required></input>
               </div>
+
               <div className="center">
                 <input type="email" name="email" id="email" placeholder="Email" size={40} required></input>
               </div>
+
               <div>
                 <input type="password" name="password" id="password" placeholder="Password" size={17} required></input>
                 <input type="password" name="password" id="confirm-password" placeholder="Confirm Password" size={17} required></input>
               </div>
+
               <button type="submit" className="center create-account">CREATE ACCOUNT</button>
+              
+              <div className="center google-section-s">
+                <button type="submit" className="google-s">
+                  <img id="google-img" src={google}  style={{ width: "2rem", height: "auto" }}></img> 
+                  Sign up with Google</button> 
+              </div>
+                      
             </form>
+            
+           
 
           </div>
 
