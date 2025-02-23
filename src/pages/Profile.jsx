731240589a0c9d3b0326b8644cb2c1a9ddd709a9
aneeshaa1 <acharyannie@gmail.com>
2025-02-23 02:13:38 -0500

    show navbar on profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,10 @@
+import "../pages/Profile.css";
+import Navbar from '../pages/Navbar.jsx';
+
 function Profile () {
     return(
         <div>
+            <Navbar></Navbar>
             <h1>Profile Page!</h1>
         </div>
     );
