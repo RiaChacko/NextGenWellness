1f0f0e96efda8ae47fe29a00143a9833f6bbc3f7
Ria Chacko <chacko.ria2003@gmail.com>
2025-02-24 16:48:36 -0500

    Added Motivation page with the navbar

---
+++ src/pages/Motivation.jsx
@@ -0,0 +1,15 @@
+
+import "../pages/Motivation.css"
+import Navbar from "./Navbar";
+
+function Motivation () {
+    return(
+        <div className="motivation-pg-container">
+            <Navbar/>
+        </div>
+
+    );
+    
+
+}
+export default Motivation;
