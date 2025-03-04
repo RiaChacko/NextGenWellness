31623eb1bc8400d2f9bbaccf0dc9a2b055efb7ec
aneeshaa1 <acharyannie@gmail.com>
2025-03-04 12:27:25 -0500

    log page setup

---
+++ src/pages/Log.jsx
@@ -0,0 +1,17 @@
+import "../pages/Log.css";
+import Navbar from "./Navbar";
+
+function Log() {
+
+    return(
+        <div>
+            <Navbar/>
+            <div className="log-container">
+                <h1>LOG DATA AND WORKOUTS</h1>
+            </div>
+        </div>
+    );
+
+}
+
+export default Log;
