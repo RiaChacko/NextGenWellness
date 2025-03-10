98da633c11207a57141da27b15713f3c2757582a
Ria Chacko <chacko.ria2003@gmail.com>
2025-03-10 13:16:30 -0400

    Added a page for recommended activities

---
+++ src/pages/RecommendedActivities.jsx
@@ -0,0 +1,13 @@
+import "../pages/RecommendedActivities.css"
+import Navbar from "./Navbar";
+function RecommendedActivities(){
+
+    return(
+        <div className="recommended-container">
+            <Navbar/>
+        </div>
+    );
+
+}
+
+export default RecommendedActivities;
