071c4249113a1e6e8ed1dd19ee3b6e7cd8e94cad
Ria Chacko <chacko.ria2003@gmail.com>
2025-03-21 00:57:38 -0400

    Added basic layout for features section more styling is needed

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,6 +3,10 @@ import "./Landing.css";
 import { Link, useNavigate } from "react-router-dom";
 import svglogo from "../assets/Icon.svg";
 import heroPic from "../assets/landingpic2.png";
+import dashboard from "../assets/dashboard.png";
+import motivation from "../assets/motivation.png";
+import fitnessplan from "../assets/fitnessplan.png";
+import goalsetting from "../assets/goalsetting.png";
 function Landing() {
 
     const navigate = useNavigate();
@@ -23,7 +27,7 @@ function Landing() {
                 </div>
                 <div className="nav-links">
                     <a>Home</a>
-                    <a>About us</a>
+                    <a href="#features">Our Features</a>
                    <Link to="/signup"><a>Sign up</a></Link> 
                    <Link to="/login"><a>Login</a></Link> 
                     <a>Contact Us</a>
@@ -38,7 +42,40 @@ function Landing() {
             </div>
 
             <div className="features">
-                <h2>Our Features</h2>
+                <h2 id="features">Our Features</h2>
+                <p>Discover the powerful features that make our interactive dashboard the ultimate tool for your fitness journey. Designed to help you stay motivated, track your progress, and achieve your goals, our dashboard brings all your fitness needs into one easy-to-use platform.</p>
+                <div className="features-list">
+                    <div className="individual-feature">
+                        <img src={dashboard}></img>
+                        <div className="title-description-feature">
+                        <h2>INTERACTIVE DASHBOARD</h2>
+                        <p>A sleek, easy-to-navigate interface that provides a clear view of your workouts, performance, and achievements. Everything you need is just a click away.</p>
+                        </div>
+                    </div>
+                    <div className="individual-feature">
+                        <div className="title-description-feature-b">
+                        <h2>MOTIVATION & SUPPORT</h2>
+                        <p>Stay motivated with daily challenges, fitness quotes, and community engagement. Join our fitness community for extra support and encouragement along the way.</p>
+                        </div>
+                        <img src={motivation}></img>
+                    </div>
+
+                    <div className="individual-feature">
+                        <img src={fitnessplan}></img>
+                        <div className="title-description-feature">
+                        <h2>PERSONALIZED FITNESS PLANS</h2>
+                        <p>Get a tailored workout plan based on your fitness level and goals, whether you’re aiming to lose weight, build muscle, or improve endurance.</p>
+                        </div>
+                    </div>
+
+                    <div className="individual-feature">
+                        <div className="title-description-feature-b">
+                        <h2>GOAL SETTING</h2>
+                        <p>Achieving your fitness goals has never been easier. Our Goal Setting feature is designed to help you stay focused, motivated, and on track to reach your full potential.</p>
+                        </div>
+                        <img src={goalsetting}></img>
+                    </div>
+                </div>
             </div>
 
 
