ba32f6f0772414417b976db463bbe5d66cdc1fff
Ria Chacko <chacko.ria2003@gmail.com>
2025-04-10 13:22:24 -0400

    Cleaned up code

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,3 @@
-// Importing necessary libraries and functionality
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { auth } from "./firebaseConfig";
@@ -8,16 +7,11 @@ import {
   GoogleAuthProvider,
 } from "firebase/auth";
 
-// Importing logos
 import logo from "../assets/login-logo.png";
 import "../pages/Login.css";
 import googlelogo from "../assets/google-lg.png";
 
-// Implements login functionality
 function Login () {
-
-
-  // Setting variable to store input values onclick
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
@@ -30,7 +24,6 @@ function Login () {
     setShowAlert(false);
     setErrorMessage("");
 
-    // Checks email and password fields for valid input
     if (!email && !password) {
       setErrorMessage("Please fill in both email and password.");
       setShowAlert(true);
@@ -50,7 +43,6 @@ function Login () {
     try {
       await signInWithEmailAndPassword(auth, email, password);
 
-      // Redirects user to dashboard page
       navigate("/dashboard");
     } catch (error) {
       setErrorMessage(error.message);
@@ -58,7 +50,6 @@ function Login () {
     }
   };
 
-  // Implementing ‘login with google’ feature for ease
   const handleGoogleSignIn = async () => {
     setShowAlert(false);
     setErrorMessage("");
@@ -66,7 +57,6 @@ function Login () {
     try {
       await signInWithPopup(auth, googleProvider);
       
-      // Redirects user to dashboard page
       navigate("/dashboard");
     } catch (error) {
       setErrorMessage(error.message);
@@ -96,15 +86,11 @@ function Login () {
 
         <div className="text-input-l center-l">
           <form onSubmit={handleEmailSignIn}>
-            {/* <div className="center-l">
-              <input type="name" name="name" id="name" placeholder="Name" size={40} required></input>
-            </div> */}
             <div className="center-l">
               <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" size={40} required></input>
             </div>
             <div>
               <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" size={40} required></input>
-              {/* <input type="password" name="password" id="confirm-password" placeholder="Confirm Password" size={17} required></input> */}
             </div>
             <button
                 type="button"
@@ -120,10 +106,6 @@ function Login () {
               
               
             </div>
-            {/* <div className="sign-in-google">
-              <img src={googlelogo}/>
-              
-            </div> */}
             <div className="center google-section-s">
             <button onClick={handleGoogleSignIn} className="google-s">
               <img id="google-img" src={googlelogo}  style={{ width: "2rem", height: "auto" }}></img>
@@ -133,59 +115,6 @@ function Login () {
           </form>
 
         </div>
-
-
-        
-    
-              {/* <div>
-                <h2>HELLO</h2>
-              <div>
-                  <img  alt="Your Company"/>
-                  <h2>Register for Account</h2>
-          </div>
-
-          <div>
-              <form action="#" method="POST">
-          <div>
-              <label for="email">Email address</label>
-              <div>
-                <input type="email" name="email" id="email" autocomplete="email" required/>
-              </div>
-            </div>
-            <div>
-              <label for="email">Name</label>
-              <div>
-                <input type="name" name="name" id="name" autocomplete="name" required/>
-              </div>
-            </div>
-
-            <div>
-              <div>
-                <label for="password">Create a Password</label>
-              </div>
-              <div>
-                <input type="password" name="password" id="password" autocomplete="current-password" required/>
-              </div>
-              
-              <div class="mt-2">
-              <label for="password">Confirm Password</label>
-                <input type="password" name="password" id="confirmpassword" autocomplete="current-password" required/>
-              </div>
-              
-            </div>
-
-            <div>
-              <button type="submit">Sign Up</button>
-            </div>
-          </form>
-
-          <p>
-            Already a Member? 
-            <Link to="/login">Login</Link>
-          </p>
-        </div>
-      </div> */}
-
     </div>
   );
 }
